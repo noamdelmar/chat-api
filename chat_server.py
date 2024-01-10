a5465891d9fcc0fa89f3501db6b613349e3c4138
Noam Delmar <noamdelmar@h-MacBook-Air-sl-Noam.local>
2024-01-10 13:04:22 +0200

    rooms working !!!!

--- chat_server.py
+++ chat_server.py
@@ -12,20 +12,20 @@ class ChatServer:
     def __init__(self, port):
         self.host = HOST
         self.port = port
-        self.clients = []
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.rooms = {}  # Dictionary to store room information
         self.shutdown_flag = threading.Event()
 
     def start(self):
         try:
-            self.server_socket.bind((self.host, self.port))
-            self.server_socket.listen()
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_socket.bind((self.host, self.port))
+            server_socket.listen()
 
             print(f"Server listening on {self.host}:{self.port}")
             logging.info(f"Server listening on {self.host}:{self.port}")
 
             while not self.shutdown_flag.is_set():
-                client_socket, client_address = self.server_socket.accept()
+                client_socket, client_address = server_socket.accept()
                 client_thread = threading.Thread(target=self.handle_client, args=(client_socket, client_address))
                 client_thread.start()
 
@@ -34,10 +34,8 @@ class ChatServer:
 
     def shutdown(self):
         self.shutdown_flag.set()
-        self.server_socket.close()
         print("Server closed.")
         logging.info("Server closed.")
-
 
     def handle_client(self, client_socket, client_address):
         try:
@@ -63,37 +61,41 @@ class ChatServer:
             client_socket.send(response.encode('utf-8'))
 
             # Receive the username from the client
-            # username = 'Oliver'
-            username = self.get_username(client_socket)
+            user_data = self.get_user_data(client_socket)
+            username = user_data[0]
+            room = user_data[1]
+
+            # Create the room if it doesn't exist
+            if room not in self.rooms:
+                self.rooms[room] = []
+
+            # Add the client to the room
+            self.rooms[room].append((username, client_socket))
 
             # Continue with the rest of your chat application logic
-            self.broadcast(f"{username} has joined the chat.", client_socket)
+            self.broadcast(f"{username} has joined the chat. To room {room}", client_socket, room)
 
-            logging.info(f"New connection from {client_address} - {username}")
-
-            self.clients.append((username, client_socket))
+            logging.info(f"New connection from {client_address} - {username} (Room: {room})")
 
             while True:
                 message = self.receive_message(client_socket)
                 if not message:
                     break
-                self.broadcast(f"{username}: {message}", client_socket)
+                self.broadcast(f"{username}: {message}", client_socket, room)
 
         except ConnectionResetError:
             pass
 
         finally:
-            connected_usernames = self.get_usernames()
-            print(f"Connected users: {', '.join(connected_usernames)}")
-            self.remove_client(username, client_socket)
-            self.broadcast(f"{username} has left the chat.", client_socket)
+            self.remove_client(username, room, client_socket)
+            # connected_usernames = self.get_usernames(room)
+            # print(f"Connected users in Room {room}: {', '.join(connected_usernames)}")
+            self.broadcast(f"{username} has left the chat.", client_socket, room)
 
-    def get_username(self, client_socket):
-        try:            
-            # Receive the WebSocket message frame
+    def get_user_data(self, client_socket):
+        try:
             frame = client_socket.recv(2048)
-            
-            # Extract the payload from the frame
+
             payload_length = frame[1] & 127
             mask_start = 2
             if payload_length == 126:
@@ -102,32 +104,31 @@ class ChatServer:
             elif payload_length == 127:
                 payload_length = int.from_bytes(frame[2:10], byteorder='big')
                 mask_start = 10
-            
+
             mask = frame[mask_start:mask_start + 4]
             data_start = mask_start + 4
-            
-            # Unmask the payload
+
             payload = bytearray()
             for i in range(payload_length):
                 payload.append(frame[data_start + i] ^ mask[i % 4])
-            
+
             decoded_payload = payload.decode('utf-8')
-            
             data = json.loads(decoded_payload)
 
-            # Check if the received message has the expected 'type' field
-            if 'type' in data and data['type'] == 'username':
-                return data['username']
+            if 'type' in data and data['type'] == 'user_data':
+                return data['username'], data['room']
             else:
-                # Handle unexpected message format
                 return None
+
         except (ConnectionResetError, ValueError, json.JSONDecodeError):
-            # Handle errors when receiving or decoding the message
             return None
 
-    def remove_client(self, username, client_socket):
-        client_socket.close()
-        self.clients.remove((username, client_socket))
+    def remove_client(self, username, room, client_socket):
+        if room in self.rooms:
+            room_clients = self.rooms[room]
+            client_socket.close()
+            room_clients = [(u, c) for u, c in room_clients if c != client_socket]
+            self.rooms[room] = room_clients
 
     def receive_message(self, client_socket):
         try:
@@ -152,14 +153,17 @@ class ChatServer:
 
         except (ConnectionResetError, ValueError):
             return None
-    
-    def broadcast(self, message, sender_socket):
-        for _, client_socket in self.clients:
-            if client_socket != sender_socket:
-                try:
-                    WebSocket.send_message(client_socket, message)
-                except:
-                    self.remove_client(_, client_socket)
-    
+
+   
+    def broadcast(self, message, sender_socket, room):
+        if room in self.rooms:
+            room_clients = self.rooms[room]
+            for _, client_socket in room_clients:
+                if client_socket != sender_socket:
+                    try:
+                        WebSocket.send_message(client_socket, message)
+                    except:
+                        self.remove_client(_, room, client_socket)
+
     def get_usernames(self):
         return [username for username, _ in self.clients]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, jsonify
-from flask_socketio import SocketIO, emit
-from flask_cors import CORS
+from flask_socketio import SocketIO
+from flask_cors import CORS  # Import CORS
 import threading
 from chat_server import ChatServer
 from routes import routes
@@ -11,9 +11,9 @@ load_dotenv()
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-CORS(app)
+CORS(app)  # Enable CORS for all routes
 
-PORT = int(os.environ.get('PORT', 5050))
+PORT = int(os.environ.get('PORT', 8080))
 
 server = ChatServer(PORT)
 app.register_blueprint(routes)
